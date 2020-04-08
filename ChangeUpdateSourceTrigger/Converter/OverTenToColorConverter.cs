c38cf25ae91e6b641cbf3a08174bc63780a81d4d
gytjd0907 <gytjd0907@xinics.com>
2020-04-08 17:57:23 +0900

    First Commit

---
+++ ChangeUpdateSourceTrigger/Converter/OverTenToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ChangeUpdateSourceTrigger.Converter
+{
+    class OverTenToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool overTen = (bool)value;
+            if (overTen)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Transparent);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChangeUpdateSourceTrigger/Model/SomeModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeUpdateSourceTrigger.Model
+{
+    public class SomeModel : INotifyPropertyChanged
+    {
+        private string someText { get; set; }
+        public string SomeText
+        {
+            get { return someText; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    OverTen = true;
+                }
+                else
+                {
+                    OverTen = false;
+                }
+                someText = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool overTen;
+        public bool OverTen
+        {
+            get { return overTen; }
+            set
+            {
+                overTen = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string name = null)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
